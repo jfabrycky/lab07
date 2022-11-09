3c3f641d9abfc17a28b045e1b284cb0f45f6434e
jfabrycky <112512196+jfabrycky@users.noreply.github.com>
2022-11-08 19:59:38 -0600

    testing event listener

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function init(){
 //add your javascrip between these two lines of code
-
+  function clickToAlert(){
+    function greet(){
+      alert('Hello');
+    }
+  }
+}
 
 
 
@@ -9,3 +14,4 @@ function init(){
 
 
 window.addEventListener('load', init);
+entrybutton.addEventListener('click', clickToAlert)
